fe3d259d38a3eee9b8435582c52a49b079672fe5
Joshua1056 <joshuashanebrown2014@gmail.com>
2024-01-03 19:58:43 +1300

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,56 @@
+import sys
+
+guideinput = input("Would you like to see the calculator guide? y/n\n")
+#guide
+guide = '''
++ for addition,
+- for subtraction,
+* for multiplication,
+/ for division,
+% for percentage,
+** for expontation,
+// for floor division,
+Type exit on any input to exit program. 
+'''
+if guideinput == 'y':
+    print(guide)
+    
+inputfirst = input("Enter first calculation number ...\n")
+#exit for inputfirst
+if inputfirst == 'exit':
+    sys.exit("Aborted.")
+    
+inputoperator = input("Enter operator for calculation ...\n")
+#exit for inputoperator
+if inputoperator == 'exit':
+    sys.exit("Aborted.")
+    
+inputlast = input("Enter last calculation number ...\n")
+#exit for inputlast
+if inputlast == "exit":
+    sys.exit("Aborted.")
+    
+inputfirstint = int(inputfirst)
+inputlastint = int(inputlast)
+print("")
+print("Your total is...")
+if inputoperator == '+': # operator checking & calculations
+    print(inputfirstint + inputlastint)
+
+if inputoperator == '-':
+    print(inputfirstint - inputlastint)
+    
+if inputoperator == '*':
+    print(inputfirstint * inputlastint)
+    
+if inputoperator == '/':
+    print(inputfirstint / inputlastint)
+    
+if inputoperator == '%':
+    print(inputfirstint % inputlastint)
+    
+if inputoperator == '**':
+    print(inputfirstint ** inputlastint)
+    
+if inputoperator == '//':
+    print(inputfirstint // inputlastint)
